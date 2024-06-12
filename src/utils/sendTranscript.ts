6cfd0db8af71aa8a63411f73865a64bd32ba8314
vedantnaik09 <vedant.naik09@gmail.com>
2024-06-12 14:27:07 +0530

    Rolled back to previous, but modified prompt

--- src/utils/sendTranscript.ts
+++ src/utils/sendTranscript.ts
@@ -4,7 +4,11 @@ import { database } from "@/app/firebaseConfig";
 import axios from "axios";
 import { showWarningToast } from "./toasts";
 
-export async function sendTranscriptTo_Chatgpt_AndPushInDatabase(roomId: string, value: string, yoe?: string) {
+export async function sendTranscriptTo_Chatgpt_AndPushInDatabase(
+  roomId: string,
+  value: string,
+  yoe?: string
+) {
   try {
     if (value.length <= 10) return;
     const response = await axios.post("/api/gptAnswer2", {
@@ -31,13 +35,20 @@ export async function sendTranscriptTo_Chatgpt_AndPushInDatabase(roomId: string,
     }
   } catch (err) {
     // toast.error("Ai failed to create a Valid JSON. Ai Output : " + err);
-    console.log("error while writing data to room or chatgpt response is not a json:", err);
+    console.log(
+      "error while writing data to room or chatgpt response is not a json:",
+      err
+    );
   }
 }
-export async function sendTranscriptTo_Chatgpt4O_AndPushInDatabase(roomId: string, value: string, role: string, yoe?: string) {
+export async function sendTranscriptTo_Chatgpt4O_AndPushInDatabase(
+  roomId: string,
+  value: string,
+  yoe?: string
+) {
   try {
     if (value.length <= 10) return;
-    const response = await axios.post("/api/detectGpt4", {
+    const response = await axios.post("/api/gpt4", {
       content: value,
       roomId,
       yoe,
@@ -47,33 +58,32 @@ export async function sendTranscriptTo_Chatgpt4O_AndPushInDatabase(roomId: strin
     const responseObject = extractJson(data.llmoutput);
     console.log("RESPONSE OBJECT IS :\n", responseObject);
     if (responseObject?.itemFound) {
-      const response = await axios.post("/api/gpt4", {
-        itemFound: responseObject.itemFound,
-        type: responseObject.type,
-        question: responseObject.question,
-        yoe: responseObject.experience,
-        role: role,
-      });
-      const data = response.data;
-      const responseObjectAfterDetection = extractJson(data.llmoutput);
-      console.log("Response after detecting is:", responseObjectAfterDetection);
+      // toast.success(
+      //   `Ai Answered the Question: ${responseObject.question} type: ${responseObject.type}`
+      // );
       // put in database
       const messagesRef = ref(database, "flowofwords/" + roomId + "/messages");
       push(messagesRef, {
-        question: responseObjectAfterDetection.question,
-        answer: responseObjectAfterDetection.answer,
+        question: responseObject.question,
+        answer: responseObject.answer,
       }).then(() => console.log("message pushed in database"));
     } else {
       showWarningToast("No Question detected in the previous. Transcript");
     }
-
   } catch (err) {
     // toast.error("Ai failed to create a Valid JSON. Ai Output : " + err);
-    console.log("error while writing data to room or chatgpt response is not a json:", err);
+    console.log(
+      "error while writing data to room or chatgpt response is not a json:",
+      err
+    );
   }
 }
 
-export async function sendTranscriptTo_MISTRAL_AndPushInDatabase(roomId: string, value: string, yoe?: string) {
+export async function sendTranscriptTo_MISTRAL_AndPushInDatabase(
+  roomId: string,
+  value: string,
+  yoe?: string
+) {
   try {
     if (value.length <= 10) return;
     const response = await axios.post("/api/mistralAnswer", {
@@ -100,6 +110,9 @@ export async function sendTranscriptTo_MISTRAL_AndPushInDatabase(roomId: string,
     }
   } catch (err) {
     // toast.error("Ai failed to create a Valid JSON. Ai Output : " + err);
-    console.log("error while writing data to room or chatgpt response is not a json:", err);
+    console.log(
+      "error while writing data to room or chatgpt response is not a json:",
+      err
+    );
   }
 }
